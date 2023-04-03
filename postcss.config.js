c9d81184b8c57c66c7ccf5f714d17b8d0f4e0bd0
Lucas Bazzi <bazzilucas32@gmail.com>
2023-04-03 20:50:56 -0300

    feat(styles): add css variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import autoprefixer from 'autoprefixer';
+import stylelint from 'stylelint';
 import postcssPresetEnv from 'postcss-preset-env';
 
 export default {
     plugins: [
         autoprefixer,
+        stylelint,
         postcssPresetEnv,
     ]
   }
